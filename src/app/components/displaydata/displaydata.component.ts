4683f21013c21cf051605219ec4c85f5567ee1ee
Smitkumar Choksi <samchoksi092@gmail.com>
2017-11-08 02:28:24 -0500

    Step 4
    Sort works both ways.

--- src/app/components/displaydata/displaydata.component.ts
+++ src/app/components/displaydata/displaydata.component.ts
@@ -8,11 +8,24 @@ import { DatafetchserviceService } from '../../datafetchservice.service';
 export class DisplaydataComponent implements OnInit {
 
   dataFromServer = [];
+  sortCol = '';
+  static sortDir: number;
   constructor(public dataFetchService : DatafetchserviceService ) { }
 
   ngOnInit() {
+    DisplaydataComponent.sortDir = 1;
     this.dataFetchService.getData()
       .subscribe(data => this.dataFromServer = data);
+  }
+
+  sortData =(col: string)=>{
+    if(this.sortCol === col) DisplaydataComponent.sortDir = DisplaydataComponent.sortDir * -1;
+    this.sortCol = col;
+    this.dataFromServer.sort(function(a,b) {
+      if (a[col] > b[col]) return 1 * DisplaydataComponent.sortDir;
+      if (a[col] < b[col]) return -1 * DisplaydataComponent.sortDir;
+      return 0;
+    });
   }
 
 }
